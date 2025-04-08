95ce3b7ac741cb3dd22d5e6ec411c396c97f564a
prechtlvit <c00298335@setu.ie>
2025-04-08 04:08:17 +0100

    Added products page

--- svelte-app/src/routes/products/+page.svelte
+++ svelte-app/src/routes/products/+page.svelte
@@ -1,3 +1,4 @@
+<title>All Products - NABI</title>
 <!-- RATING STAR ICON -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
@@ -10,7 +11,7 @@
             <nav>
                 <ul id="MenuItems">
                     <li><a href="/">Home</a></li>
-                    <li><a href="/products">Products</a></li>
+                    <li><a href="">Products</a></li>
                     <li><a href="">About</a></li>
                     <li><a href="">FAQ</a></li>
                     <li><a href="">Account</a></li>
@@ -19,49 +20,22 @@
             <img src="cart.svg" alt="Shopping Cart Icon" width="30px" height="30px">
             <img src="menu.svg" alt="Burger Menu Icon" width="30px" height="30px" class="menu-icon">
         </div>
-        <div class="row">
-            <div class="col-2">
-                <h1>NABI - Where Streetwear meets Self-Improvement</h1>
-                <p>Success isn't about greatness. It's about consistency, so we're here to remind yourself to become better everyday!</p>
-                <a href="" class="button">Explore Spring Sale Now! &#8594;</a>
-            </div>
-            <div class="col-2">
-                <img src="image-1.png" alt="">
-            </div>
-        </div>
-    </div>
-</div>
-
-<!--CATEGORIES-->
-<div class="categories">
-    <div class="small-container">
-        <h2 class="title">Categories</h2>
-            <div class="row">
-                <div class="col-3">
-                    <img src="category-1.jpg" alt="">
-                    <div class="category-text">
-                        Shoes
-                    </div>
-                </div>
-                <div class="col-3">
-                    <img src="category-2.jpg" alt="">
-                    <div class="category-text">
-                        Full
-                    </div>
-                </div>
-                <div class="col-3">
-                    <img src="category-3.jpg" alt="">
-                    <div class="category-text">
-                        Other
-                    </div>
-                </div>
-            </div>
     </div>
 </div>
 
 <!-- FEATURED PRODUCTS -->
 <div class="small-container">
-    <h2 class="title">Featured Products</h2>
+    <div class="row-2">
+        <h2 class="title">Our Products</h2>
+        <select>
+            <option>Default Sorting</option>
+            <option>Low to High</option>
+            <option>High to Low</option>
+            <option>Most popular</option>
+            <option>Discounted</option>
+        </select>
+    </div>
+
     <div class="row">
         <div class="col-4">
             <img src="shirts/shirt-1.jpg" alt="">
@@ -112,7 +86,6 @@
             <p>$39.99</p>
         </div>
     </div>
-    <h2 class="title">Latest Products</h2>
     <div class="row">
         <div class="col-4">
             <img src="shirts/shirt-5.jpg" alt="">
@@ -215,72 +188,6 @@
     </div>
 </div>
 
-<!-- OFFER -->
- <div class="offer">
-    <div class="small-container">
-        <div class="row">
-            <div class="col-2">
-                <img src="category-1.jpg" alt="" class="offer-img">
-            </div>
-            <div class="col-2">
-                <p>Exclusively Available on NABI Eshop</p>
-                <h1>Cool shoes</h1>
-                <small>Shoes for the biggest G's that want to make the world better haha.</small>
-                <a href="" class="button">Buy Now &#8594;</a>
-            </div>
-        </div>
-    </div>
- </div>
-
- <!-- TESTIMONIAL -->
-  <div class="testimonial">
-    <div class="small-container">
-        <div class="row">
-            <div class="col-3">
-                <i class="fa fa-quote-left"></i>
-                <p>Would definitely recommend NABI and will definitely be ordering again.</p>
-                <div class="rating">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star-o"></i>
-                </div>
-                <img src="testimonials/person-1.jpg">
-                <h3>Marcus Epictetus</h3>
-            </div>
-
-            <div class="col-3">
-                <i class="fa fa-quote-left"></i>
-                <p>I love anything that I purchase from NABI!</p>
-                <div class="rating">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                </div>
-                <img src="testimonials/person-2.jpg">
-                <h3>Osas Uwe</h3>
-            </div>
-
-            <div class="col-3">
-                <i class="fa fa-quote-left"></i>
-                <p>NABI is exactly what I've been looking for.</p>
-                <div class="rating">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star-o"></i>
-                </div>
-                <img src="testimonials/person-3.jpg">
-                <h3>Anala Richards</h3>
-            </div>
-        </div>
-    </div>
-  </div>
-
   <!-- BRANDS -->
   <div class="brands">
     <div class="small-container">
@@ -339,7 +246,7 @@
     </div>
   </div>
 
-  <style>
+<style>
     /*Global.css content*/
     *{
     margin: 0;
@@ -431,9 +338,6 @@
         background: #ab3600;
     }
 
-    .header{
-        background: radial-gradient(#fff,#b4e9ff);
-    }
     .header .row{
         margin-top: 70px; /*Margin from navbar and the next frame*/
     }
@@ -657,5 +561,18 @@
             display: block;
             cursor: pointer;
         }
+    }
+
+    /*Sorting*/
+    .row-2{
+        justify-content: space-between;
+        margin: 100px auto 50px;
+    }
+    select{
+        border: 1px solid #ff523b;
+        padding: 5px;
+    }
+    select:focus{
+        outline: none;
     }
 </style>
