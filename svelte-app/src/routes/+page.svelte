9c5593fc3195e60f96692273cf91221f08eb1c5f
prechtlvit <c00298335@setu.ie>
2025-04-08 01:12:29 +0100

    Added Frames to Homepage

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -1,3 +1,6 @@
+<!-- RATING STAR ICON -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <div class="header">
     <div class="container">
         <div class="navbar">
@@ -5,7 +8,7 @@
                 <img src="logo_placeholder.png" width="125px">
             </div>
             <nav>
-                <ul>
+                <ul id="MenuItems">
                     <li><a href="">Home</a></li>
                     <li><a href="">Products</a></li>
                     <li><a href="">About</a></li>
@@ -14,6 +17,7 @@
                 </ul>
             </nav>
             <img src="cart.svg" alt="Shopping Cart Icon" width="30px" height="30px">
+            <img src="menu.svg" alt="Burger Menu Icon" width="30px" height="30px" class="menu-icon">
         </div>
         <div class="row">
             <div class="col-2">
@@ -36,14 +40,289 @@
                 <img src="category-1.jpg" alt="">
             </div>
             <div class="col-3">
-                <img src="category-1.jpg" alt="">
+                <img src="category-2.jpg" alt="">
             </div>
             <div class="col-3">
-                <img src="category-1.jpg" alt="">
+                <img src="category-3.jpg" alt="">
             </div>
         </div>
     </div>
 </div>
+
+<!-- FEATURED PRODUCTS -->
+<div class="small-container">
+    <h2 class="title">Featured Products</h2>
+    <div class="row">
+        <div class="col-4">
+            <img src="product-1.jpg" alt="">
+            <h4>Red Printed T-Shirt</h4>
+            <div class="rating">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+            </div>
+            <p>$50.00</p>
+        </div>
+        <div class="col-4">
+            <img src="product-1.jpg" alt="">
+            <h4>Red Printed T-Shirt</h4>
+            <div class="rating">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+            </div>
+            <p>$50.00</p>
+        </div>
+        <div class="col-4">
+            <img src="product-1.jpg" alt="">
+            <h4>Red Printed T-Shirt</h4>
+            <div class="rating">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+            </div>
+            <p>$50.00</p>
+        </div>
+        <div class="col-4">
+            <img src="product-1.jpg" alt="">
+            <h4>Red Printed T-Shirt</h4>
+            <div class="rating">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+            </div>
+            <p>$50.00</p>
+        </div>
+    </div>
+    <h2 class="title">Latest Products</h2>
+    <div class="row">
+        <div class="col-4">
+            <img src="product-1.jpg" alt="">
+            <h4>Red Printed T-Shirt</h4>
+            <div class="rating">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+            </div>
+            <p>$50.00</p>
+        </div>
+        <div class="col-4">
+            <img src="product-1.jpg" alt="">
+            <h4>Red Printed T-Shirt</h4>
+            <div class="rating">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+            </div>
+            <p>$50.00</p>
+        </div>
+        <div class="col-4">
+            <img src="product-1.jpg" alt="">
+            <h4>Red Printed T-Shirt</h4>
+            <div class="rating">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+            </div>
+            <p>$50.00</p>
+        </div>
+        <div class="col-4">
+            <img src="product-1.jpg" alt="">
+            <h4>Red Printed T-Shirt</h4>
+            <div class="rating">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+            </div>
+            <p>$50.00</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <img src="product-1.jpg" alt="">
+            <h4>Red Printed T-Shirt</h4>
+            <div class="rating">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+            </div>
+            <p>$50.00</p>
+        </div>
+        <div class="col-4">
+            <img src="product-1.jpg" alt="">
+            <h4>Red Printed T-Shirt</h4>
+            <div class="rating">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+            </div>
+            <p>$50.00</p>
+        </div>
+        <div class="col-4">
+            <img src="product-1.jpg" alt="">
+            <h4>Red Printed T-Shirt</h4>
+            <div class="rating">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+            </div>
+            <p>$50.00</p>
+        </div>
+        <div class="col-4">
+            <img src="product-1.jpg" alt="">
+            <h4>Red Printed T-Shirt</h4>
+            <div class="rating">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+            </div>
+            <p>$50.00</p>
+        </div>
+    </div>
+</div>
+
+<!-- OFFER -->
+ <div class="offer">
+    <div class="small-container">
+        <div class="row">
+            <div class="col-2">
+                <img src="category-1.jpg" alt="" class="offer-img">
+            </div>
+            <div class="col-2">
+                <p>Exclusively Available on NABI Eshop</p>
+                <h1>Cool shoes</h1>
+                <small>Shoes for the biggest G's that want to make the world better haha.</small>
+                <a href="" class="button">Buy Now &#8594;</a>
+            </div>
+        </div>
+    </div>
+ </div>
+
+ <!-- TESTIMONIAL -->
+  <div class="testimonial">
+    <div class="small-container">
+        <div class="row">
+            <div class="col-3">
+                <i class="fa fa-quote-left"></i>
+                <p>Would definitely recommend NABI and will definitely be ordering again.</p>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <img src="person-placeholder.jpg">
+                <h3>John Doe</h3>
+            </div>
+
+            <div class="col-3">
+                <i class="fa fa-quote-left"></i>
+                <p>I love anything that I purchase from NABI!</p>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <img src="person-placeholder.jpg">
+                <h3>John Doe</h3>
+            </div>
+
+            <div class="col-3">
+                <i class="fa fa-quote-left"></i>
+                <p>NABI is exactly what I've been looking for.</p>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <img src="person-placeholder.jpg">
+                <h3>John Doe</h3>
+            </div>
+        </div>
+    </div>
+  </div>
+
+  <!-- BRANDS -->
+  <div class="brands">
+    <div class="small-container">
+        <div class="row">
+            <div class="col-5">
+                <img src="favicon.png" alt="">
+            </div>
+
+            <div class="col-5">
+                <img src="favicon.png" alt="">
+            </div>
+
+            <div class="col-5">
+                <img src="favicon.png" alt="">
+            </div>
+
+            <div class="col-5">
+                <img src="favicon.png" alt="">
+            </div>
+
+            <div class="col-5">
+                <img src="favicon.png" alt="">
+            </div>
+        </div>
+    </div>
+  </div>
+
+  <!--FOOTER-->
+  <div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col-1">
+                <h3>For you and Humanity</h3>
+            </div>
+            <div class="footer-col-2">
+                <img src="logo_placeholder.png" alt="">
+            </div>
+            <div class="footer-col-3">
+                <h3>Useful Links</h3>
+                <ul>
+                    <li>Instagram</li>
+                    <li>Twitter</li>
+                    <li>Facebook</li>
+                    <li>YouTube</li>
+                    <li>Join Affiliate</li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <p class="copyright">&copy Copyright 2025 - NABI</p>
+    </div>
+  </div>
 
 <style>
     /*Global.css content*/
@@ -52,9 +331,11 @@
     padding: 0;
     box-sizing: border-box;
 
-    font-family: "Schoolbell", cursive;
-  font-weight: 400;
-  font-style: normal;
+    font-family: "Merriweather Sans", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: <weight>;
+    font-style: normal;
+
 }
 
   
@@ -152,5 +433,191 @@
         margin: auto;
         padding-left: 25px;
         padding-right: 25px;
+    }
+
+    /*Featured Product*/
+    .col-4{
+        flex-basis: 25%;
+        padding: 10px;
+        min-width: 200px;
+        margin-bottom: 50px;
+        /*Animation*/
+        transition: transform 0.5s;
+    }
+    .col-4 img{
+        width: 100%
+    }
+    .title{
+        text-align:center;
+        margin: 0 auto 80;
+        position: relative;
+        line-height: 60px;
+        color: #555;
+    }
+    .title::after{
+        content: '';
+        background: #ff523b;
+        width: 80px;
+        height: 5px;
+        border-radius: 5px;
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+
+    h4{
+        color: #555;
+        font-weight: normal;
+    }
+    .col-4 p{
+        font-size: 14px;
+    }
+    .col-4:hover{
+        transform: translateY(-5px);
+    }
+    .rating .fa{
+        color: #ff523b;
+    }
+
+    /*OFFER*/
+    .offer {
+        background: radial-gradient(#fff,#ffba97);
+        margin-top: 80px;
+        padding: 30px 0;
+    }
+    .col-2 .offer-img{
+        padding: 50px;
+    }
+    small{
+        color: #555;
+    }
+
+    /*Testimonial*/
+    .testimonial{
+        padding-top: 100px;
+    }
+    .testimonial .col-3{
+        text-align: center;
+        padding: 40px 20px;
+        box-shadow: 0 0 20px 0px rgba(0,0,0,0.25);
+        border-radius: 5%;
+        cursor: pointer;
+        /*Animation*/
+        transition: transform 0.5s;
+    }
+    .testimonial .col-3:hover{
+        transform: translateY(-10px);
+    }
+    .testimonial .col-3 img{
+        width: 50px;
+        margin-top: 20px;
+        border-radius: 50%;
+    }
+    .fa.fa-quote-left{
+        font-size: 34px;
+        color: #ff523b;
+    }
+    .col-3 p{
+        font-size: 12px;
+        margin: 12px 0;
+        color #777;
+    }
+    .testimonial .col-3 h3{
+        font-weight: 600;
+        color: #555;
+        font-size: 16px;
+    }
+
+    /*BRANDS*/
+    .brands{
+        margin: 100px auto;
+    }
+    .col-5{
+        width: 160px;
+    }
+    .col-5 img{
+        width: 100%;
+        cursor: pointer;
+        filter: grayscale(100%); /*All Images Gray*/
+    }
+    .col-5 img:hover{
+        filter: grayscale(0); /*All Images Gray*/
+    }
+
+    /*FOOTER*/
+    .footer{
+        background: #000;
+        color: #8a8a8a;
+        font-size: 14px;
+        padding: 60px 0 20px;
+    }
+
+    .footer p{
+        color: #8a8a8a;
+    }
+    .footer h3{
+        color: #fff;
+        margin-bottom: 20px;
+    }
+    .footer-col-1, .footer-col-2, .footer-col-3 {
+        min-width: 400px;
+        margin-bottom: 20px;
+    }
+    .footer-col-1{
+        flex: 1;
+        text-align: center;
+    }
+    .footer-col-2 img{
+        width: 180px;
+        margin-bottom: 20px;
+    }
+    .footer-col-3{
+        flex-basis: 12%;
+        text-align: center;
+    }
+    ul{
+        list-style-type: none;
+    }
+    .footer hr{
+        border: none;
+        background: #b5b5b5;
+        height: 1px;
+        margin: 20px 0;
+    }
+    .copyright{
+        text-align: center;
+    }
+
+    /*Menu Icon - Responsive (MOBILE)*/
+    .menu-icon{
+        width: 28px;
+        margin-left: 20px;
+        display: none;
+    }
+
+    /*Media query for menu*/
+    @media only screen and (max-width: 800px){
+        nav ul{
+            position: absolute;
+            top: 70px;
+            left: 0;
+            background: #333;
+            width: 100%;
+
+        }
+        nav ul li{
+            display: block;
+            margin-right: 50px;
+            margin-top: 10px;
+            margin-bottom: 10px;
+        }
+        nav ul li a{
+            color: #fff;
+        }
+        .menu-icon{
+            display: block;
+            cursor: pointer;
+        }
     }
 </style>
